d95f7eeaa9187f38a717a3b53f24b21e7e15b11f
CarolynPro <carol.ingabil.18@gmail.com>
2025-03-24 07:40:33 -0500

    Se agrego el Script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function validarFormulario(){
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const telefono = document.getElementById("tel").value;
+    const mensaje = document.getElementById("Mensaje").value;
+    
 
+    if (!nombre) { alert( "Ingrese campo Nombre" ); return false; }
+    if( email.length == 0 ){ alert( "Ingrese campo Correo" ); return false; }
+    if( telefono == "" ){ alert( "Ingrese campo Teléfono" ); return false; }
+    if( mensaje.length == 0 ){ alert( "Ingrese campo Correo" ); return false; }
+    
+    else{
+        // mostrarImagen(imagen);
+        alert("Registro completo");
+        return true;
+    }
+}
