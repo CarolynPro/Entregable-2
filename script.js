3a2dce794cfce5f17f0151649d1febd45dce75f3
CarolynPro <carol.ingabil.18@gmail.com>
2025-03-24 08:11:49 -0500

    Ajustando Scripts

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function validarFormulario(){
     const nombre = document.getElementById("nombre").value;
     const email = document.getElementById("email").value;
-    const telefono = document.getElementById("tel").value;
+    const celular = document.getElementById("tel").value;
     const mensaje = document.getElementById("Mensaje").value;
     
 
     if (!nombre) { alert( "Ingrese campo Nombre" ); return false; }
     if( email.length == 0 ){ alert( "Ingrese campo Correo" ); return false; }
-    if( telefono == "" ){ alert( "Ingrese campo Teléfono" ); return false; }
+    if( celular == "" ){ alert( "Ingrese campo Teléfono" ); return false; }
     if( mensaje.length == 0 ){ alert( "Ingrese campo Correo" ); return false; }
     
     else{
-        // mostrarImagen(imagen);
         alert("Registro completo");
         return true;
     }
